a0119dbd11731ac6d8a215ef4280e559ff70c8b6
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-07 16:46:48 -0400

    updated look of site

---
+++ src/main/java/me/moosecanswim/crabbysite/Controller/controller.java
@@ -0,0 +1,12 @@
+package me.moosecanswim.crabbysite.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
